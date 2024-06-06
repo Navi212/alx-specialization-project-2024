303a837e68b85477b5716673762f8e3ca682cfb2
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2024-06-06 08:20:10 +0000

    Debugged some syntax error and added description of the root test(test suite).

--- test/test.js
+++ test/test.js
@@ -9,45 +9,45 @@ const connectDB = require('./server/config/db');
 const { isActiveRoute } = ('./server/helpers/routerHelpers');
 
 
+describe('TEST SUITE FOR THE NODE APP', () => {
+	describe('TESTING isActiveRoute FUNCTION...', () => {
+		it('Should return true if the current route matches the provided route', () => {
+			const route = '/home';
+			const currentRoute = '/home';
+			const result = isActiveRoute(route, currentRoute);
 
-describe('TESTING isActiveRoute FUNCTION...', () => {
-	it('Should return true if the current route matches the provided route', () => {
-		const route = '/home';
-		const currentRoute = '/home';
-		const result = isActiveRoute(route, currentRoute);
+			assert.strictEqual(result,true);
+			});
 
-		assert.strictEqual(result,true);
-	});
+		it('should return false if the current route does not match the provided route', () => {
+			const route = '/home';
+			const currentRoute = '/about';
+			const result = isActiveRoute(route, currentRoute);
 
-	it('should return false if the current route does not match the provided route', () => {
-		const route = '/home';
-		const currentRoute = '/about';
-		const result = isActiveRoute(route, currentRoute);
-
-		assert.strictEqual(result, false);
-	});
-});
-
-
-//Integration test for express app
-describe('EXPRESS APP', () =>{
-	it('Should start the server without any errors', () => {
-		request(app)
-		.get('/')
-		.expect(200)
-		.end((err) => {
-			if (err) return done(err);
-			done();
+			assert.strictEqual(result, false);
+			});
 		});
-	});
 
-	it('Should return 404 for undefined routes', (done) => {
-		request(app)
-		.get('/pseudo-route')
-		.expect(404)
-		.end((err) => {
-			if (err) return done(err);
-			done();
+	//Integration test for express app
+	describe('TESTING EXPRESS APP', () =>{
+		it('Should start the server without any errors', () => {
+			request(app)
+			.get('/')
+			.expect(200)
+			.end((err) => {
+				if (err) return done(err);
+				done();
+			});
+		});
+
+		it('Should return 404 for undefined routes', (done) => {
+			request(app)
+			.get('/pseudo-route')
+			.expect(404)
+			.end((err) => {
+				if (err) return done(err);
+				done();
+			});
 		});
 	});
 });
